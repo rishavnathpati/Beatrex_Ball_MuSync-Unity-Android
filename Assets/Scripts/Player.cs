106852e3e2d055153887b99ee6dbcb1b0088858d
rishavnathpati <patirishavnath@gmail.com>
2020-06-05 22:29:45 +0530

    Added first stair

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    Rigidbody2D rb;
+    public float jumpForce = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
