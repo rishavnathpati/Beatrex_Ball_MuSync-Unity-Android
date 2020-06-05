fd14f18067af1b598d3073bcd2c9221661e9bad5
rishavnathpati <patirishavnath@gmail.com>
2020-06-06 03:07:55 +0530

    Camera follows player added

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    public float smoothTime = 0.3f;
+    public float yOffset;
+    
+    Vector2 velocity = Vector2.zero;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 targetPos = player.transform.TransformPoint(new Vector3(0, yOffset));
+        
+        if(targetPos.y<transform.position.y)
+        {
+            return;
+        }
+
+        targetPos = new Vector2(0, targetPos.y);
+        transform.position = Vector2.SmoothDamp(transform.position,targetPos,ref velocity,smoothTime);
+        transform.position = new Vector3(transform.position.x, transform.position.y, -10);
+    }
+}
