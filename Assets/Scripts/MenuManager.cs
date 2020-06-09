5e76b207b98973f8a74eba6d16a43627ed08a208
Rishav Nath Pati <patirishavnath@gmail.com>
2020-06-09 11:28:40 -0700

    Menu UI updated

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -8,10 +8,26 @@ public class MenuManager : MonoBehaviour
     public void PlayButton()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        Time.timeScale = 1;
     }
 
     public void QuitButton()
     {
-        Application.Quit();
+        Application.Quit(); 
+    }
+
+    public void PauseButton()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void ResumeButton()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void HomeButton()
+    {
+        SceneManager.LoadScene(0);
     }
 }
